647c60f979a8c4d63105797fcea3fd803077ff82
angrythunderbird <headcrab.albert@gmail.com>
2021-03-23 00:33:55 +0200

    feat: add function to pass what-season test (without FAIL point)

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const notDate = 'Unable to determine the time of year!';
+  const seasonsArr = ['winter', 'spring', 'summer', 'autumn'];
+  let month = date.getMonth();
+
+  if (isNaN(date.getTime())) {
+    return notDate;
+  }
+
+  if (month < 2 || month === 11) {
+    return seasonsArr[0];
+  } else if (month < 5) {
+    return seasonsArr[1];
+  } else if (month < 8) {
+    return seasonsArr[2];
+  } else if (month < 11) {
+    return seasonsArr[3];
+  }
 };
